a46c559cf510540a52a340a209251400a113ca6d
Cesar Matsubayashi <cesar.matsubayashi@gmail.com>
2025-02-14 18:17:17 -0300

    feat: add about section

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import Header from "../Header/Header";
 import Main from "../Main/Main";
+import About from "../About/About";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Header />
         <Main />
       </div>
+      <About />
     </div>
   );
 }
